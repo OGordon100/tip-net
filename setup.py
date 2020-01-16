523eb4f1feaf644d718e60a3f7f9b243fc1eb218
Oliver Gordon <oliver.gordon@nottingham.ac.uk>
2020-01-16 13:20:29 +0000

    Take actions

--- setup.py
+++ setup.py
@@ -2,5 +2,5 @@ from setuptools import setup
 
 setup(name='tip_net',
       version='0.0.1',
-      install_requires=['gym']#And any other dependencies required
-)
+      install_requires=['gym', 'nOmicron']  #And any other dependencies required
+      )
--- tip_net/envs/tip_net_env.py
+++ tip_net/envs/tip_net_env.py
@@ -1,16 +1,63 @@
+import sys
+
 import gym
-from gym import error, spaces, utils
-from gym.utils import seeding
+import numpy as np
+from gym import spaces
+from nOmicron.microscope import IO
+from nOmicron.microscope.conditioning import tip_pulse, tip_crash
+
+sys.path.insert(1, '/home/mltest1/tmp/pycharm_project_510')
+from MicroscopeFuncs.coarse_improvements import ImprovementController
 
 
 class TipNet(gym.Env):
-  metadata = {'render.modes': ['human']}
+    metadata = {'render.modes': ['human']}
+    np.seterr(divide='ignore', invalid='ignore')
 
-  def __init__(self):
-    ...
-  def step(self, action):
-    ...
-  def reset(self):
-    ...
-  def render(self, mode='human', close=False):
-    ...
+    def __init__(self):
+        self.controller_name = "TipNet"
+        IO.connect()
+        self.scan_bias_voltage = -3.5
+
+        self.improver = ImprovementController(person_name=self.controller_name)
+        self.improver.saveloc = "/home/mltest1/tmp/pycharm_project_510/Data/ImprovementSessions2/"
+        self.improver.reset(destroy_tip=False)
+
+        self.observation_space = spaces.Box(low=0, high=1, shape=(17,), dtype=np.float32)
+        self.action_space = spaces.Discrete(24)
+
+    def step(self, action):
+        self._take_action(action)
+        obs = self.improver.step()
+        reward = self.calc_reward(obs)
+
+        done = False
+
+        return obs, reward, done, {}
+
+    def _take_action(self, action):
+
+        action = np.argmax(action)
+
+        if action == 0:  # do nothing
+            pass
+        elif action == 1:
+            self.improver.scan_pos.big_move_and_reset_size()
+        elif action == 2:
+            self.improver.scan.pos.reduce_scan_size(scandata=self.improver.scan_prog.scan_matrix_raw[0, :, :])
+        elif 3 <= action <= 13:
+            intensities = np.arange(-10, 12, 2)
+            pulse_intensity = intensities[action]
+            tip_pulse(voltage=pulse_intensity, time=200e-3)
+        elif action <= 23:
+            distances = np.arange(0.2, 2.2, 0.2)
+            press_distance = distances[action]
+            tip_crash(press_distance)
+        else:
+            raise RuntimeError("Output action is too high!!!!")
+
+    def reset(self):
+        self.improver.reset()
+
+    def render(self, mode='human', close=False):
+        self.improver.render()
